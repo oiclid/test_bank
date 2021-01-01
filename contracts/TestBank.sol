aec5ebf297af53f93588f21eddeac29147f5a741
oiclid <oiclid@tutanota.com>
2021-01-01 20:51:18 +0100

    fixed bug in TestBabk.sol

--- contracts/TestBank.sol
+++ contracts/TestBank.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.7.0;
 
 contract TestBank {
     uint8 private clientNumber;
-    mapping (address => uint) private balances;
+    mapping (address => uint8) private balances;
     address public owner;
 
   // Log the event about a deposit being made by an address and its amount
-    event LogDepositMade(address indexed accountAddress, uint amount);
+    event LogDepositMade(address indexed accountAddress, uint8 amount);
 
     // Constructor is "payable" so it can receive the initial funding of 30, 
     // required to reward the first 5 clients
@@ -21,7 +21,7 @@ contract TestBank {
     /// @notice Enroll a customer with the bank, 
     /// giving the first 5 of them 5 ether as reward
     /// @return The balance of the user after enrolling
-    function enroll() public returns (uint) {
+    function enroll() public returns (uint8) {
         if (clientNumber < 5) {
             clientNumber++;
             balances[msg.sender] = 5 ether;
@@ -31,14 +31,14 @@ contract TestBank {
 
     /// @notice Deposit ether into bank, requires method is "payable"
     /// @return The balance of the user after the deposit is made
-    function deposit() public payable returns (uint) {
+    function deposit() abstract payable returns (uint8) {
         balances[msg.sender] += msg.value;
         emit LogDepositMade(msg.sender, msg.value);
         return balances[msg.sender];
     }
 
     
-    function withdraw(uint withdrawAmount) public returns (uint remainingBal) {
+    function withdraw(uint8 withdrawAmount) public returns (uint8 remainingBal) {
         // Check enough balance available, otherwise just return balance
         if (withdrawAmount <= balances[msg.sender]) {
             balances[msg.sender] -= withdrawAmount;
@@ -49,12 +49,12 @@ contract TestBank {
 
     /// @notice Just reads balance of the account requesting, so "constant"
     /// @return The balance of the user
-    function balance() public view returns (uint) {
+    function balance() public view returns (uint8) {
         return balances[msg.sender];
     }
 
     /// @return The balance of the Simple Bank contract
-    function depositsBalance() public view returns (uint) {
+    function depositsBalance() public view returns (uint8) {
         return address(this).balance;
     }
 }
